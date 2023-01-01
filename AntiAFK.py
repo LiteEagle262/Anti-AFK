89aff38dc3d36e027569967e1b64b90b190de09d
LiteEagle <48303729+LiteEagle262@users.noreply.github.com>
2023-01-01 18:41:50 -0500

    V2

--- AntiAFK.py
+++ AntiAFK.py
@@ -1,5 +1,6 @@
-import pyautogui,ctypes
+import pyautogui, ctypes
 from time import sleep
+
 ctypes.windll.kernel32.SetConsoleTitleW("Nest Anti-AFK")
 print("""
        d8888          888    d8b            d8888 8888888888 888    d8P  
@@ -10,18 +11,25 @@ print("""
   d88P   888 888  888 888    888       d88P   888 888        888  Y88b   
  d8888888888 888  888 Y88b.  888      d8888888888 888        888   Y88b  
 d88P     888 888  888  "Y888 888     d88P     888 888        888    Y88b 
-                                                                         
-                                                                         
+
+
                  Auto Starting In 10 Seconds
                     Close this to stop it!                                                       
 """)
+
+
 def start():
     sleep(10)
     while True:
-        sleep(5)
-        pyautogui.keyDown(' ')
-        pyautogui.keyUp(' ')
-        sleep(5)
-        pyautogui.keyDown(' ')
-        pyautogui.keyUp(' ')
+        try:
+            sleep(5)
+            pyautogui.keyDown(' ')
+            pyautogui.keyUp(' ')
+            sleep(5)
+            pyautogui.keyDown(' ')
+            pyautogui.keyUp(' ')
+        except:
+            start()
+            break
+
 start()
