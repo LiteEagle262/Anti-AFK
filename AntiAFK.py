79aa376dddb5351061f4cba792fefdc2b84114cd
LiteEagle <48303729+LiteEagle262@users.noreply.github.com>
2023-01-02 16:16:22 -0500

    Update AntiAFK.py

--- AntiAFK.py
+++ AntiAFK.py
@@ -1,8 +1,14 @@
-import pyautogui, ctypes
+import keyboard, os
 from colorama import Fore
 from time import sleep
 
-ctypes.windll.kernel32.SetConsoleTitleW("Nest Anti-AFK")
+enabled = False
+last_state = False
+count = 0
+
+os.system("title Nest | AntiAFK")
+os.system("cls")
+
 print(Fore.YELLOW + """
        d8888          888    d8b            d8888 8888888888 888    d8P  
       d88888          888    Y8P           d88888 888        888   d8P   
@@ -11,24 +17,47 @@ print(Fore.YELLOW + """
    d88P  888 888 "88b 888    888        d88P  888 888        8888888b    
   d88P   888 888  888 888    888       d88P   888 888        888  Y88b   
  d8888888888 888  888 Y88b.  888      d8888888888 888        888   Y88b  
-d88P     888 888  888  "Y888 888     d88P     888 888        888    Y88b 
-                 Auto Starting In 10 Seconds
-                    Close this to stop it!                                                       
+d88P     888 888  888  "Y888 888     d88P     888 888        888    Y88b              
+
+                        Made By liteeagle.me                                                                             
 """)
 
+toggle_button = input(Fore.GREEN + "Please Type your Toggle Key (EX: F1): ")
+
+os.system("cls")
+
+print(Fore.YELLOW + f"""
+       d8888          888    d8b            d8888 8888888888 888    d8P  
+      d88888          888    Y8P           d88888 888        888   d8P   
+     d88P888          888                 d88P888 888        888  d8P    
+    d88P 888 88888b.  888888 888         d88P 888 8888888    888d88K     
+   d88P  888 888 "88b 888    888        d88P  888 888        8888888b    
+  d88P   888 888  888 888    888       d88P   888 888        888  Y88b   
+ d8888888888 888  888 Y88b.  888      d8888888888 888        888   Y88b  
+d88P     888 888  888  "Y888 888     d88P     888 888        888    Y88b 
+                   Press {toggle_button} to turn on Anti-AFK.
+                        Close this to stop it.
+                        Made By liteeagle.me                                               
+""")
 
 def start():
-    sleep(10)
     while True:
         try:
-            sleep(5)
-            pyautogui.keyDown(' ')
-            pyautogui.keyUp(' ')
-            sleep(5)
-            pyautogui.keyDown(' ')
-            pyautogui.keyUp(' ')
+            keyboard.press_and_release('a')
+            sleep(1)
+            keyboard.press_and_release('s')
+            sleep(1)
+            keyboard.press_and_release('space')
+            sleep(1)
+            keyboard.press_and_release('space')
+            sleep(1)
+            return start()
         except:
-            start()
-            break
+            return start()
 
-start()
+while True:
+    key_down = keyboard.is_pressed(toggle_button)
+    if key_down:
+        print(Fore.RED + "Starting Anti-AFK...")
+        start()
+        break
