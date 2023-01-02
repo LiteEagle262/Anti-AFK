91a6557ab053d16d567804b5d61b23c94d76cf69
LiteEagle <48303729+LiteEagle262@users.noreply.github.com>
2023-01-02 11:54:33 -0500

    Update AntiAFK.py

--- AntiAFK.py
+++ AntiAFK.py
@@ -1,8 +1,9 @@
 import pyautogui, ctypes
+from colorama import Fore
 from time import sleep
 
 ctypes.windll.kernel32.SetConsoleTitleW("Nest Anti-AFK")
-print("""
+print(Fore.YELLOW + """
        d8888          888    d8b            d8888 8888888888 888    d8P  
       d88888          888    Y8P           d88888 888        888   d8P   
      d88P888          888                 d88P888 888        888  d8P    
@@ -11,8 +12,6 @@ print("""
   d88P   888 888  888 888    888       d88P   888 888        888  Y88b   
  d8888888888 888  888 Y88b.  888      d8888888888 888        888   Y88b  
 d88P     888 888  888  "Y888 888     d88P     888 888        888    Y88b 
-
-
                  Auto Starting In 10 Seconds
                     Close this to stop it!                                                       
 """)
